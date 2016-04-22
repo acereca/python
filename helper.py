5621e873731a2783789bfc39da47e82e7ffb1962
Patrick Nisble <tigerauge96@live.de>
2016-04-22 13:00:55 +0200

    add helper.py

---
+++ helper.py
@@ -0,0 +1,22 @@
+#######################################################################
+###     This script is used as my goto-file for dataanalysis        ###
+###     during the pap2 at Uni Heidelberg. Mostly reused functions  ###
+###     for preventing redundance.                                  ###
+###                                                                 ###
+###     By:     Patrick Nisble                                      ###
+###     Ver:    1                                                   ###
+###     PyVer:  2.3+ / 3.3+                                         ###
+#######################################################################
+
+# todos
+
+# imports
+import numpy as _np
+import matplotlib.pyplot as _plt
+import matplotlib.mlab as _mlab
+
+#definitions
+
+#load data from file
+def importData(fileName):
+
