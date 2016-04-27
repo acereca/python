f70c94347214145a69b3306db9e1a9dd2320657c
Patrick Nisble <tigerauge96@live.de>
2016-04-27 20:40:33 +0200

    ALDA/in: add distGeo function, change to take arguments and save coord type in dict entry 0

--- ALDA/in.py
+++ ALDA/in.py
@@ -16,6 +16,10 @@ def readTspData(filename):
         for elem in clean:
             if elem.startswith("DIMENSION"):
                 dim = int(nnum.sub("",elem))
+            elif elem.startswith("EDGE"):
+                coord = (elem.split())[1]
+
+        tupel.append((0,coord))
 
         for item in clean:
             for x in range(1,dim + 1):
@@ -32,17 +36,44 @@ def elem(elem,node):
     elif elem == "y":
         return node[1][1]
 
-def distEucl(n1,n2):
+def distEuc(n1,n2):
     import numpy as np
     dx = (elem("x",n1)-elem("x",n2))
     dy = (elem("y",n1)-elem("y",n2))
     return np.sqrt(dx**2+dy**2)
 
 def distGeo(n1,n2):
-    return
+    import numpy as np
+    ## node 1
+    deg = int(elem("x", n1))
+    minu = elem("x", n1)
+    latitude1 = np.PI * (deg + 5.0 * min / 3.0) / 180
+    deg = int(elem("y", n1))
+    minu = elem("y", n1)
+    longitude1 = np.PI * (deg + 5.0 * min / 3.0) / 180
+    ## node 2
+    deg = int(elem("x", n1))
+    minu = elem("x", n1)
+    latitude2 = np.PI * (deg + 5.0 * min / 3.0) / 180
+    deg = int(elem("y", n1))
+    minu = elem("y", n1)
+    longitude2 = np.PI * (deg + 5.0 * min / 3.0) / 180
 
-tspDict = readTspData("data/berlin52.tsp")
+    rad = 6378.388
+    q1 = np.cos(longitude1 - longitude2 )
+    q2 = np.cos(latitude1 - latitude2 )
+    q3 = np.cos(latitude1 + latitude2 )
+    dij = int(rad * np.arccos(.5*((1.0+q1)*q2 - (1.0+q1)*q3)) + 1.0)
 
-print(tspDict)
+    return dij
 
-print(elem("x",tspDict[1]))
+    
+## Main Block
+
+import sys
+
+if (len(sys.argv) == 2 and sys.argv[1].startswith("data")):
+    tspDict = readTspData(sys.argv[1])
+    print(tspDict)
+else:
+    print("you gave me something else than a tsplib formatted file, pls retry :|")
